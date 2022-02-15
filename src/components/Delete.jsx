59345e8aac3d926560562530ed61ffa0e32162f4
Alexander Sopin <alex.sopin0803@gmail.com>
2022-02-15 19:58:34 +0700

    added className

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Delete = (props) => {
-  return <button onClick={props.onClick}>X</button>;
+const Delete = props => {
+  return (
+    <button className="delete-item" onClick={props.onClick}>
+      X
+    </button>
+  );
 };
 
 export default Delete;
